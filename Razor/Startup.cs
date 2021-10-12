9ba4d1582c4c177cfaa913a96f8457e2d392bc96
DmitryDutov <dmitry_dutov@inbox.ru>
2021-10-13 01:53:11 +0300

    Очистка класса Startup

--- Razor/Startup.cs
+++ Razor/Startup.cs
@@ -13,12 +13,11 @@ namespace Razor
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
 
+            //Настройка для возможности использования UseMvcWithDefaultRoute();
             services.AddControllersWithViews(mvcOtions =>
             {
                 mvcOtions.EnableEndpointRouting = false;
@@ -26,26 +25,9 @@ namespace Razor
 
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseMvcWithDefaultRoute();
-
-            ////В режиме разработчика может переадресовывать на страницу с ошибкой
-            //if (env.IsDevelopment())
-            //{
-            //    app.UseDeveloperExceptionPage();
-            //}
-
-            //app.UseRouting(); //Вместо app.UseMvcWithDefaultRoute()
-
-            //app.UseEndpoints(endpoints =>
-            //{
-            //    endpoints.MapGet("/", async context =>
-            //    {
-            //        await context.Response.WriteAsync("Hello World!");
-            //    });
-            //});
         }
     }
 }

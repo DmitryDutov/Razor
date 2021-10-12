5beff322c022378d483daa1fa27d47fe6aa0a6b8
DmitryDutov <dmitry_dutov@inbox.ru>
2021-10-13 01:47:56 +0300

    Добавьте файлы проекта.

---
+++ Razor/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Razor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Razor.Controllers
+{
+    public class HomeController: Controller
+    {
+        public ViewResult Index()
+        {
+            Product myProduct = new Product
+            {
+                 ProductId      = 1
+                , Name          = "Lodka"
+                , Description   = "A boat for one person"
+                , Price         = 275M
+            };
+
+            return View(myProduct);
+        }
+    }
+}
